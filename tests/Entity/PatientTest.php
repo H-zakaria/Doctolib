834583fd85a470ad5c0f13da7655c0e7560e41fe
H-zakaria <hamdi.zakaria@protonmail.com>
2021-06-14 15:22:15 +0200

    test-patient

---
+++ tests/Entity/PatientTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\test\Entity;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\Patient;
+
+use function PHPUnit\Framework\assertEquals;
+
+class PatientTest extends TestCase
+{
+
+  public function testSetNom()
+  {
+    $patient = new Patient;
+    $patient->setNom('jp');
+
+    $nom = $patient->getNom();
+    assertEquals('jp', $nom, 'enjfojeoijfief');
+  }
+  public function testGetNom()
+  {
+    $patient = new Patient;
+    $patient->setNom('jp');
+
+    $nom = $patient->getNom();
+    assertEquals('jp', $nom, 'enjfojeoijfief');
+  }
+  public function testSetPrenom()
+  {
+    $patient = new Patient;
+    $patient->setPrenom('jp');
+
+    $Prenom = $patient->getPrenom();
+    assertEquals('jp', $Prenom, 'enjfojeoijfief');
+  }
+  public function testGetPrenom()
+  {
+    $patient = new Patient;
+    $patient->setPrenom('jp');
+
+    $Prenom = $patient->getPrenom();
+    assertEquals('jp', $Prenom, 'enjfojeoijfief');
+  }
+  public function testSetAdresse()
+  {
+    $patient = new Patient;
+    $patient->setAdresse('jp');
+
+    $Adresse = $patient->getAdresse();
+    assertEquals('jp', $Adresse, 'enjfojeoijfief');
+  }
+  public function testGetAdresse()
+  {
+    $patient = new Patient;
+    $patient->setAdresse('jp');
+
+    $Adresse = $patient->getAdresse();
+    assertEquals('jp', $Adresse, 'enjfojeoijfief');
+  }
+}
