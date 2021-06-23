812807d20112274cf43e3fe24cde7e8f2c6cabdd
H-zakaria <hamdi.zakaria@protonmail.com>
2021-06-23 15:45:49 +0200

    ajout de ville et rue dans rdv Entity

--- src/Entity/Rdv.php
+++ src/Entity/Rdv.php
@@ -35,6 +35,17 @@ class Rdv
    */
   private $praticien;
 
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $ville;
+
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $rue;
+
+
   public function getId(): ?int
   {
     return $this->id;
@@ -74,5 +85,29 @@ class Rdv
     $this->praticien = $praticien;
 
     return $this;
+  }
+
+  public function getVille(): ?string
+  {
+      return $this->ville;
+  }
+
+  public function setVille(string $ville): self
+  {
+      $this->ville = $ville;
+
+      return $this;
+  }
+
+  public function getRue(): ?string
+  {
+      return $this->rue;
+  }
+
+  public function setRue(string $rue): self
+  {
+      $this->rue = $rue;
+
+      return $this;
   }
 }
